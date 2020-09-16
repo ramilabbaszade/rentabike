93558b10bb58cc0f3a613c344f502f1c793b4ee8
ramilabbaszade <abbasovramil65@gmail.com>
2020-09-16 19:55:19 +0400

    user-profiles-modernized

--- src/User/components/UserItem.js
+++ src/User/components/UserItem.js
@@ -1,26 +1,51 @@
-import React from "react";
-import Avatar from "../../shared/components/UIElements/Avatar";
-import BikeItem from "../../Bikes/components/BikeItem";
+import React from 'react'
+import Avatar from '../../shared/components/UIElements/Avatar'
+import { Link } from 'react-router-dom'
+import BikeItem from '../../Bikes/components/BikeItem'
 
-import "./UserItem.css";
+import './UserItem.css'
 
 const UserItem = (props) => {
   return (
-    <div className='user-item'>
+    <div className="user_container">
       <div className="user-header">
         <div className="user-header__i">
           <Avatar creatorImg={props.avatar} className="avatar-large" />
-          <div className="user-header__info">
-            <h2> {props.fullName} </h2>
-            <p className="user-header__city"> <i className='fas fa-map-marker-alt'></i> {props.city} </p>
-            <p className='user-header__bio'> {props.bio} </p>
+          <div className="user-header__confirmed">
+            <div className="user-header__info_icon-container">
+              <i class="far fa-smile"></i>
+              <span> Yeni üzv </span>
+            </div>
+            <div className="user-header__info_icon-container">
+              <i class="fas fa-check"></i>
+              <span>Hesab təsdiqləndi</span>
+            </div>
+          </div>
+
+          <div className="user-header__editBtn">
+            <Link to='/'>
+              Mesaj yaz
+            </Link>
           </div>
         </div>
       </div>
 
       <div className="user-body">
-        <h3>Shared Bikes</h3>
+        <section className="user-body__section1">
+          <h1> {props.fullName} </h1>
+          <small className="small-text">Qoşuldu: 01.08.2020</small>
+        </section>
+        <section className="user-body__section2">
+          <h1 className="user-body__section2_title">Haqqında</h1>
+          <p >{props.bio}</p>
+          <div className="icon-container">
+            <i class="fas fa-street-view"></i>
+            <div>{props.city}</div>
+          </div>
+        </section>
+        <hr className="bike-bottom-line" />
         <div className="user-body__shared-bikes">
+          <h2 className='user-body__title'>Aktiv elanlar</h2>
           {props.bikes.length === 0 ? (
             <h4>Bike not found</h4>
           ) : (
@@ -34,13 +59,24 @@ const UserItem = (props) => {
                   price={bike.price.first}
                   creator={bike.creator}
                   image={bike.images[0]}
-                />
+                  actionicons
+                >
+                  <div className="bike-item__action-icons">
+                    <Link to="/" className="bike-item__action-icon icon-edit">
+                      <i class="fas fa-pen"></i>
+                    </Link>
+                    <Link to='/' className="bike-item__action-icon icon-remove">
+                      <i class="fas fa-trash-alt"></i>
+                    </Link>
+                  </div>
+                </BikeItem>
               }))}
+
         </div>
       </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default UserItem;
+export default UserItem
