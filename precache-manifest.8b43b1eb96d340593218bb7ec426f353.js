834b9a0f8974a2f13616283bf24c798fef4bb59a
ramilabbaszade <abbasovramil65@gmail.com>
2020-09-27 00:02:44 +0400

    Updates

---
+++ precache-manifest.8b43b1eb96d340593218bb7ec426f353.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37ea409d964873635484a914f8615f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89376acc98e947fd7b91",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "22fdf21d064da2b7c6d6",
+    "url": "/static/css/main.8e8fecd8.chunk.css"
+  },
+  {
+    "revision": "89376acc98e947fd7b91",
+    "url": "/static/js/2.b58fc36c.chunk.js"
+  },
+  {
+    "revision": "a79fe65b4e2196566329e9c6926859e9",
+    "url": "/static/js/2.b58fc36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fdf21d064da2b7c6d6",
+    "url": "/static/js/main.23c11d78.chunk.js"
+  },
+  {
+    "revision": "3c8e6970fabb9ee18735",
+    "url": "/static/js/runtime-main.94275045.js"
+  },
+  {
+    "revision": "f363425011ff365454ee5b62fe54c994",
+    "url": "/static/media/bike-helmet.f3634250.png"
+  },
+  {
+    "revision": "c5c3a93db8082f379107b45a1b032787",
+    "url": "/static/media/bike1con.c5c3a93d.png"
+  },
+  {
+    "revision": "8c1f8aee5abe5d35042c065bd262c92c",
+    "url": "/static/media/music-and-multimedia.8c1f8aee.png"
+  },
+  {
+    "revision": "270487e0dc9473c30221711247eee9f2",
+    "url": "/static/media/refletor.270487e0.png"
+  },
+  {
+    "revision": "79d15d95495846e6e4f8e7240706b52c",
+    "url": "/static/media/road-2562568_1920.79d15d95.jpg"
+  },
+  {
+    "revision": "5a0d5b28c94d285108b0dde87748118b",
+    "url": "/static/media/undraw_Ride_a_bicycle_2yok.5a0d5b28.png"
+  },
+  {
+    "revision": "12a3cfe62c4be84e72a4ca7af7cbe5f9",
+    "url": "/static/media/undraw_chat_1wo5.12a3cfe6.png"
+  },
+  {
+    "revision": "b8848291f177160635cee8f41f4cea99",
+    "url": "/static/media/undraw_fill_forms_yltj.b8848291.svg"
+  },
+  {
+    "revision": "332ed386e485ba12c363597a94d31895",
+    "url": "/static/media/undraw_map_1r69.332ed386.png"
+  },
+  {
+    "revision": "b06b649d7f89a3ff40cf25e1950e4063",
+    "url": "/static/media/undraw_sign_in_e6hj.b06b649d.svg"
+  }
+]);
